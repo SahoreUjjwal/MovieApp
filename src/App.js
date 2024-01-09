d66e63351504e7f39a4c3e53c371296f0be4ef59
ujjwal shaore <sahoreujjwal@gmail.com>
2024-01-10 02:03:07 +0530

    Added add cart and cart count functionality with navbar css. Shifted state to app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,132 @@
+import { Component } from "react";
 import MovieList from "./MovieList";
-function App() {
-  return (
-      <>
-        <h1>Movie App</h1>
-        <MovieList/>
-      </>
-  );
+import Navbar from "./Navbar";
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state={
+        movies:[{
+            title:"The Avengers",
+            plot:"superhero movie",
+            price:89,
+            rating:8.9,
+            stars:3,
+            fav:false,
+            addCart:false,
+            isAdded:false
+        },
+        {
+            title:"The Avengers",
+            plot:"superhero movie",
+            price:89,
+            rating:8.9,
+            stars:3,
+            fav:false,
+            addCart:false,
+            isAdded:false
+        },
+        {
+            title:"The Avengers",
+            plot:"superhero movie",
+            price:89,
+            rating:8.9,
+            stars:3,
+            fav:false,
+            addCart:false,
+            isAdded:false
+        },
+        {
+            title:"The Avengers",
+            plot:"superhero movie",
+            price:89,
+            rating:8.9,
+            stars:3,
+            fav:false,
+            addCart:false,
+            isAdded:false
+        }],
+      cartCount:0        
+    }
+}
+addStars=(movie)=>{
+    //there are two ways to set state
+    //first
+    // this.setState({
+    //     stars:this.state.stars + 0.5
+    // });
+    //Second - Used when previous state value is required
+    const mid = this.state.movies.indexOf(movie);
+    const {movies} = this.state;
+    if(movies[mid].stars>0)
+    {
+        movies[mid].stars +=0.5;
+        this.setState({
+            movies:movies
+        })  
+    }
+    
+}
+decreaseStars(movie){
+    // this.setState({
+    //     stars:this.state.stars - 0.5
+    // });
+
+    const mid = this.state.movies.indexOf(movie);
+    const {movies} = this.state;
+    if(movies[mid].stars>0)
+    {
+        movies[mid].stars -=0.5;
+        this.setState({
+            movies:movies
+        })
+    }   
+}
+toggleFavourite=(movie)=>{
+    const mid = this.state.movies.indexOf(movie);
+    const {movies} = this.state;
+    movies[mid].fav =!movies[mid].fav 
+    this.setState({
+        movies:movies
+    })
+}
+toggleCart=(movie)=>{
+    const mid = this.state.movies.indexOf(movie);
+    const {movies} = this.state;
+    if(movies[mid].isAdded)
+    {
+      movies[mid].addCart =!movies[mid].addCart 
+      movies[mid].isAdded=!movies[mid].isAdded ;
+      this.setState({
+        movies:movies,
+        cartCount:this.state.cartCount-1
+    })
+    }
+    else{
+      movies[mid].addCart =!movies[mid].addCart 
+      movies[mid].isAdded=!movies[mid].isAdded;
+      this.setState({
+        movies:movies,
+        cartCount:this.state.cartCount+1
+    })
+    }
+   
+    
+}
+  
+  render(){
+      return (
+        <>
+          <Navbar cartCount={this.state.cartCount}/>
+          <MovieList movies={this.state.movies} 
+                addStars={this.addStars} 
+                decreaseStars={this.decreaseStars.bind(this)} 
+                toggleCart={this.toggleCart} 
+                toggleFavourite = {this.toggleFavourite}/>
+        </>
+    );
+  }
+  
 }
 
 export default App;
--- src/MovieList.js
+++ src/MovieList.js
@@ -1,90 +1,10 @@
 import React from "react";
 import MovieCard from "./MovieCard";
-export default class MovieList extends React.Component{
-    constructor(){
-        super();
-        this.state={
-            movies:[{
-                title:"The Avengers",
-                plot:"superhero movie",
-                price:89,
-                rating:8.9,
-                stars:3,
-                fav:false,
-                addCart:false
-            },
-            {
-                title:"The Avengers",
-                plot:"superhero movie",
-                price:89,
-                rating:8.9,
-                stars:3,
-                fav:false,
-                addCart:false
-            },
-            {
-                title:"The Avengers",
-                plot:"superhero movie",
-                price:89,
-                rating:8.9,
-                stars:3,
-                fav:false,
-                addCart:false
-            },
-            {
-                title:"The Avengers",
-                plot:"superhero movie",
-                price:89,
-                rating:8.9,
-                stars:3,
-                fav:false,
-                addCart:false
-            }]        
-        }
-    }
-    addStars=(movie)=>{
-        //there are two ways to set state
-        //first
-        // this.setState({
-        //     stars:this.state.stars + 0.5
-        // });
-        //Second - Used when previous state value is required
-        const mid = this.state.movies.indexOf(movie);
-        const {movies} = this.state;
-        movies[mid].stars +=0.5;
-        this.setState({
-            movies:movies
-        })
-        
-    }
-    decreaseStars(movie){
-        // this.setState({
-        //     stars:this.state.stars - 0.5
-        // });
-
-        const mid = this.state.movies.indexOf(movie);
-        const {movies} = this.state;
-        movies[mid].stars -=0.5;
-        this.setState({
-            movies:movies
-        })
-    }
-    toggleFavourite(){
-        this.setState({
-            fav:!this.state.fav
-        })
-    }
-    toggleCart(){
-        this.setState({
-            addCart:!this.state.addCart
-        })
-    }
-   
-    render(){
-        const {movies} = this.state;
+export default function MovieList(props){
+        const {movies,addStars,decreaseStars,toggleCart,toggleFavourite} = props;
         return(<>
-            {movies.map((movie)=>(<MovieCard movie={movie} addStars = {this.addStars} decreaseStars={this.decreaseStars.bind(this)}/>))}
+            {movies.map((movie)=>(<MovieCard movie={movie} addStars = {addStars} decreaseStars={decreaseStars} toggleCart={toggleCart} toggleFavourite={toggleFavourite}/>))}
             {/*addStars and decreaseStars are declared diffrently to demonstrate two different methods of passing a function */}
-        </>     
-    )}
+        </>    ) 
+    
 }
